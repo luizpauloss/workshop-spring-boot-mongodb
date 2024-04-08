ef91acafc5a85fdf238c604fa053cecadd20ad25
Luiz Paulo <luizstyle@hotmail.com>
2024-04-08 10:39:51 -0300

    Consulta simples com query methods

--- src/main/java/com/luizpaulodev/workshopmongo/resources/PostResource.java
+++ src/main/java/com/luizpaulodev/workshopmongo/resources/PostResource.java
@@ -1,26 +1,18 @@
 package com.luizpaulodev.workshopmongo.resources;
 
-import java.net.URI;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.luizpaulodev.workshopmongo.domain.Post;
-import com.luizpaulodev.workshopmongo.domain.User;
-import com.luizpaulodev.workshopmongo.dto.UserDTO;
-import com.luizpaulodev.workshopmongo.repository.PostRepository;
+import com.luizpaulodev.workshopmongo.resources.util.URL;
 import com.luizpaulodev.workshopmongo.services.PostService;
-import com.luizpaulodev.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/posts")
@@ -33,5 +25,12 @@ public class PostResource {
  	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
